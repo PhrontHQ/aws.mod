ecb39b87ec62d2b2e32b4f177de597f1f5fa8286
Benoit Marchant <marchant@mac.com>
2025-09-15 22:39:21 -0700

    update cloud-computing.mod -> cloud.mod

--- data/main.mod/service/s3-data-service.js
+++ data/main.mod/service/s3-data-service.js
@@ -11,9 +11,9 @@ var S3Client,
     //SyntaxInOrderIterator = (require) ("mod/core/frb/syntax-iterator").SyntaxInOrderIterator,
     DataOperation = require("mod/data/service/data-operation").DataOperation,
     crypto = require("crypto"),
-    BucketDescriptor = require("cloud-computing.mod/data/model/object-storage/bucket.mjson").montageObject,
-    ObjectDescriptor = require("cloud-computing.mod/data/model/object-storage/object.mjson").montageObject,
-    ExpiringObjectDownloadDescriptor = require("cloud-computing.mod/data/model/object-storage/expiring-object-download.mjson").montageObject;
+    BucketDescriptor = require("cloud.mod/data/model/object-storage/bucket.mjson").montageObject,
+    ObjectDescriptor = require("cloud.mod/data/model/object-storage/object.mjson").montageObject,
+    ExpiringObjectDownloadDescriptor = require("cloud.mod/data/model/object-storage/expiring-object-download.mjson").montageObject;
 
 
 
